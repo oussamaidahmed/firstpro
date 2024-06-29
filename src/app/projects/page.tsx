703ef9301a95596115dfb82b30d1e238fc555c1e
oussamaidahmed <idhmedo@gmail.com>
2024-06-29 17:45:57 +0100

    projects

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,14 +1,20 @@
+import Projects_data from "@/components/Projects_data";
 import React from "react";
 
 export default function page() {
   return (
-    <div className=" relative top-[-100px] flex flex-col justify-center items-center gap-2  h-screen ">
-      <h1 className="text-white text-5xl font-bold -tracking-widest font-league-gothic ">
-      MY PROJECTS
-      </h1>
-      <h1 className="text-white text-2xl font-normal tracking-widest font-league-spartan ">
-      MADE WITH LOVE
-      </h1>
+    <div className="relative top-[-100px]">
+      <div className="  flex flex-col justify-center items-center gap-2  h-screen ">
+        <h1 className="text-white text-5xl font-bold -tracking-widest font-league-gothic ">
+          MY PROJECTS
+        </h1>
+        <h1 className="text-white text-2xl font-normal tracking-widest font-league-spartan ">
+          MADE WITH LOVE
+        </h1>
+      </div>
+      <div className=" ">
+        <Projects_data />
+      </div>
     </div>
   );
 }
--- src/components/Projects_data.tsx
+++ src/components/Projects_data.tsx
@@ -1,7 +1,51 @@
-import React from 'react'
+const Projects = [
+  {
+    id: 1,
+    name: "Project 1",
+    description: "Project 1 description",
+    link: "https://www.google.com",
+  },
+  {
+    id: 2,
+    name: "Project 2",
+    description: "Project 2 description",
+    link: "https://www.google.com",
+  },
+  {
+    id: 3,
+    name: "Project 3",
+    description: "Project 3 description",
+    link: "https://www.google.com",
+  },
+  {
+    id: 4,
+    name: "Project 4",
+    description: "Project 4 description",
+    link: "https://www.google.com",
+  },
+  {
+    id: 5,
+    name: "Project 5",
+    description: "Project 5 description",
+    link: "https://www.google.com",
+  },
+  {
+    id: 6,
+    name: "Project 6",
+    description: "Project 6 description",
+    link: "https://www.google.com",
+  },
+];
 
 export default function Projects_data() {
   return (
-    <div>Projects_data</div>
-  )
+    <div className="grid grid-cols-3 gap-6 bg-white justify-center items-center text-center p-10 ">
+      {Projects.map((project) => (
+        <div key={project.id }  className="border border-black p-6">
+          <h1>{project.name}</h1>
+          <p>{project.description}</p>
+        </div>
+      ))}
+    </div>
+  );
 }
